00c09c2962deb5c23ec435a8c45b06bd439952db
yaguilar130 <yosselynaguilarbernal@gmail.com>
2025-02-17 21:29:03 -0400

    Clase circulo.py

---
+++ Clase circulo.py
@@ -0,0 +1,10 @@
+class Circulo:
+    def __init__(self, centro, radio):
+        self.centro = centro
+        self.radio = radio
+
+    def ostentacion(self):
+        return f
+
+    def dibujar_circulo(self):
+        print(self.ostentacion())
